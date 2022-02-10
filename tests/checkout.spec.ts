eccc7e919d6cf183c442c4181d6545058aaca394
DenisGrigorenko <thedreamon@yandex.ru>
2022-02-10 22:48:51 +0300

    Добавление первого теста для Корзины

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,19 +1,16 @@
 import { test, expect, Locator } from '@playwright/test';
 
-test.describe("Нажатие на кнопку Купить", () => {
+const checkoutButton = `xpath=//button[@class="lc-button lc-button_theme_base lc-button_size_m lc-button_type_button lc-header__cart-button"]`
+
+test.describe("Проверка Корзины", () => {
     test.beforeEach(async ({ page, baseURL }) => {
         await page.goto(baseURL);
-        await page.click(`button >> text=Купить`, {strict: false});
         
     });
 
-    test('Добавление в корзину первого тавара 1шт', async({ page }) => {
-        expect(await page.waitForSelector(`button >> text="В корзине: 1"`, {state: 'visible'})).toBeTruthy();
-    });
-
-    test('Добавление в корзину первого тавара 2шт', async({ page }) => {
-        await page.click(`button >> text=В корзине: 1`, {strict: false});
-        expect(await page.waitForSelector(`button >> text="В корзине: 2"`, {state: 'visible'})).toBeTruthy();
+    test.only('Пустая корзина', async ({ page }) => {
+        await page.click(checkoutButton); 
+        expect(await page.waitForSelector(`text="Корзина пуста"`, {state: 'visible'})).toBeTruthy();
     });
 
 });

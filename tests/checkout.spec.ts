5aba831530c0452e5d1147dc2a5e72ea757d62b2
DenisGrigorenko <thedreamon@yandex.ru>
2022-02-10 22:08:35 +0300

    Добавление в корзину одного товара

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,19 +1,15 @@
 import { test, expect, Locator } from '@playwright/test';
 
-const buyButton =`xpath=//div[@class="lc-good__button-wrapper"]//span[@class="lc-button__text"]`;
-let buyButtonLocator: Locator;
-
 test.describe("Проверка Корзины", () => {
     test.beforeEach(async ({ page, baseURL }) => {
         await page.goto(baseURL);
-        buyButtonLocator = page.locator(buyButton).first();
        // await page.locator(checkoutButton).first().click();
         
     });
 
-    test.only('Добавление в корзину первого тавара 1шт', async ({ page }) => {
-        await buyButtonLocator.click();
-        expect(buyButtonLocator.textContent()).toBe('В корзине: 1');
-    })
-    
+    test.only('Добавление в корзину первого тавара 1шт', async({ page }) => {
+        await page.click(`button >> text=Купить`, {strict: false});
+        expect(await page.waitForSelector(`button >> text="В корзине: 1"`, {state: 'visible'})).toBeTruthy();
+    });
+
 });

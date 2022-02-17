fabd96c67f5c7076b41d4cdcb11e1b320d6d38fe
DenisGrigorenko <thedreamon@yandex.ru>
2022-02-17 03:39:22 +0300

    Добавление тесте с добовлением таваров в карзину

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -6,7 +6,7 @@ import { test, expect, Locator } from '@playwright/test';
 const checkoutButton = `xpath=//button[@class="lc-button lc-button_theme_base lc-button_size_m lc-button_type_button lc-header__cart-button"]`;
 const checkoutModalImage =`xpath=//div[@class="lc-header__cart-popup lc-header__cart-popup_is-expanded"]`;
 const checkoutBuyButton = `xpath=//button[@class="turbo-button turbo-button_theme_text-blue turbo-button_size_none turbo-cart-item__remove"]`;
-const modalProductInfo = `xpath=//div[@class="turbo-cart-item__info"]`;
+const modalProductInfo = `xpath=//div[@class="turbo-cart-item__info"]`; 
 
 test.describe("Проверка Корзины", () => {
     test.beforeEach(async ({ page, baseURL }) => {
@@ -42,7 +42,7 @@ test.describe("Проверка Корзины", () => {
         expect(await imageCheckOutThreeProduct.screenshot()).toMatchSnapshot('checkOutThreeProduct.png');
     });
     
-    test.only('Добавление ', async ({ page }) => {
+    test('Удаление товаров в корзине', async ({ page }) => {
         const buyButton = page.locator(`button >> text=Купить`);
         await buyButton.nth(0).click();
         await buyButton.nth(0).click();
@@ -53,5 +53,18 @@ test.describe("Проверка Корзины", () => {
         await buyButtonClose.nth(0).click();
         await buyButtonClose.nth(0).click();
         expect(await page.waitForSelector(modalProductInfo, {state: 'hidden'})).toBeFalsy();
+    });
+
+    test.only('Добавление товаров внутри корзины', async ({ page }) => {
+        await page.click(`button >> text=Купить`, {strict: false});
+        await page.click(checkoutButton);
+        const buttonCheckoutAdd = `xpath=//button[@class="turbo-button turbo-button_theme_none turbo-button_size_none turbo-amount-picker__btn turbo-amount-picker__btn_type_inc"]`;
+        const addButton = page.locator(buttonCheckoutAdd);
+        await addButton.nth(0).click();
+        const numberOfProducts =`xpath=//input[@class="turbo-amount-input turbo-amount-picker__input"]`
+        const productsNumberText = page.locator(numberOfProducts);
+        expect(productsNumberText).toBe(value="2");
+        //expect(await page.waitForSelector(numberOfProducts, {state: 'visible'})).toBeTruthy();
+        await page.pause();
     });
 });

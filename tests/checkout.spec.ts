6d432a7ac6a4e661e7ae9220463f9cd08fdfab8b
DenisGrigorenko <thedreamon@yandex.ru>
2022-02-17 03:06:33 +0300

    Добавление теста с удалением товара

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -5,6 +5,8 @@ import { test, expect, Locator } from '@playwright/test';
 >>>>>>> 63d9173 (Последнее сохранение Проверки корзины)
 const checkoutButton = `xpath=//button[@class="lc-button lc-button_theme_base lc-button_size_m lc-button_type_button lc-header__cart-button"]`;
 const checkoutModalImage =`xpath=//div[@class="lc-header__cart-popup lc-header__cart-popup_is-expanded"]`;
+const checkoutBuyButton = `xpath=//button[@class="turbo-button turbo-button_theme_text-blue turbo-button_size_none turbo-cart-item__remove"]`;
+const modalProductInfo = `xpath=//div[@class="turbo-cart-item__info"]`;
 
 test.describe("Проверка Корзины", () => {
     test.beforeEach(async ({ page, baseURL }) => {
@@ -32,7 +34,7 @@ test.describe("Проверка Корзины", () => {
         expect(await imageCheckOutTwoProduct.screenshot()).toMatchSnapshot('checkOutTwoProduct.png');
     });
 
-    test.only('Добавление всех присудствующих товаров на странице', async ({ page }) => {
+    test('Добавление всех присудствующих товаров на странице', async ({ page }) => {
         const buttonsChechout=await page.$$('.lc-add-to-cart');
         await Promise.all(buttonsChechout.map((button) => button.click()));
         await page.click(checkoutButton);
@@ -40,4 +42,16 @@ test.describe("Проверка Корзины", () => {
         expect(await imageCheckOutThreeProduct.screenshot()).toMatchSnapshot('checkOutThreeProduct.png');
     });
     
+    test.only('Добавление ', async ({ page }) => {
+        const buyButton = page.locator(`button >> text=Купить`);
+        await buyButton.nth(0).click();
+        await buyButton.nth(0).click();
+        await buyButton.nth(0).click();
+        await page.click(checkoutButton);
+        const buyButtonClose = page.locator(checkoutBuyButton);
+        await buyButtonClose.nth(0).click();
+        await buyButtonClose.nth(0).click();
+        await buyButtonClose.nth(0).click();
+        expect(await page.waitForSelector(modalProductInfo, {state: 'hidden'})).toBeFalsy();
+    });
 });
